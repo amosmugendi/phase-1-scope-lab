d49f24d8e3326e02355955126c1d8bfd20e03c76
amosmugendi <amosmugendi07@gmail.com>
2024-03-21 09:45:22 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Write your solution in this file!
+var customerName= "bob";
+
+const leastfavouriteCustomer='Jonie';
+
+console.log(customerName)
+
+function upperCaseCustomerName(){
+    customerName= customerName.toUpperCase()
+    console.log(customerName)
+}
+
+function setBestCustomer(){
+    bestCustomer= 'not bob';
+    console.log(bestCustomer)
+}
+
+
+function overwriteBestCustomer(){
+    bestCustomer ='maybe bob'
+    console.log(bestCustomer)
+}
+function changeLeastFavoriteCustomer(){
+leastfavouriteCustomer= 'Waweru';
+
+    console.log(leastfavouriteCustomer);
+}
+changeLeastFavoriteCustomer()
